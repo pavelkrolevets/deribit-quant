a6890b7a4355439956f4744ac842d0f1d2902978
pavelkrolevets <pavelkrolevets@gmail.com>
2019-09-11 14:55:33 +0800

    add: options charting

--- app/components/Deribit/index.js
+++ app/components/Deribit/index.js
@@ -29,8 +29,20 @@ import FormHelperText from '@material-ui/core/FormHelperText';
 import Select from '@material-ui/core/Select';
 import Typography from '@material-ui/core/Typography';
 import { fade } from '@material-ui/core/styles/colorManipulator';
+import {
+  XYPlot,
+  XAxis,
+  YAxis,
+  ChartLabel,
+  HorizontalGridLines,
+  VerticalGridLines,
+  LineSeries,
+  LineSeriesCanvas,
+  Crosshair
+} from 'react-vis';
 import { compute_bsm } from '../../utils/http_functions';
-import {XYPlot, XAxis, YAxis, HorizontalGridLines, LineSeries} from 'react-vis';
+import {curveCatmullRom} from 'd3-shape';
+
 
 const styles = theme => ({
   root: {
@@ -55,12 +67,16 @@ const styles = theme => ({
 });
 
 
-
-
 class Deribit extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      option_values: [],
+      chart_data_current:[],
+      chart_data_at_zero: [],
+      trade_price: 10,
+      crosshairValues: [],
+      yDomain: [-20, 20]
     };
   }
   // async componentWillMount(){
@@ -75,41 +91,101 @@ class Deribit extends Component {
   //   });
   // }
 
-  async computeBSM (){
-    let data = [];
-    for (let i=0; i<200; i+10){
-      data.push(JSON.stringify({S0: i, K:120, T:0.3, r: 0.03, sigma: 0.7}))
-    }
-    await compute_bsm(this.props.user.token, 'call', data)
-      .then(response=> console.log(response))
+  async plot(){
+    await this.computeBSM(10, 0.3)
+      .then(result=>this.setState({chart_data_current: result}));
+    await this.computeBSM(10, 0.001)
+      .then(result=>this.setState({chart_data_at_zero: result}));
   }
+
+  async computeBSM (trade_price, T) {
+    let data = [];
+    let S0 = [];
+    let chart_data=[];
+
+    for (let i=0; i < 200; i += 10){
+      data.push(JSON.stringify({S0: i, K:120, T:T, r: 0.03, sigma: 0.7}));
+      S0.push(i)
+    }
+    console.log(data);
+    await compute_bsm(this.props.user.token, 'call', data)
+      .then(response=> {console.log(response);
+      this.setState({option_values: response.data.option_values });
+      });
+
+    let y_range = [];
+    for (let i=0; i<S0.length; i++) {
+      chart_data.push({x: S0[i], y: (this.state.option_values[i]-trade_price)});
+      y_range.push((this.state.option_values[i]-trade_price))
+    }
+    this.setState({yDomain: [Math.min(...y_range)-20, Math.max(...y_range)+20]});
+
+    return chart_data;
+  }
+
+  _onMouseLeave = () => {
+    this.setState({crosshairValues: []});
+  };
+
+  _onNearestX = (value, {index}) => {
+    this.setState({crosshairValues: [this.state.chart_data_current[index]]});
+  };
 
   render() {
     const {classes} = this.props;
+    const {useCanvas} = this.state;
+    const content = useCanvas ? 'TOGGLE TO SVG' : 'TOGGLE TO CANVAS';
+    const Line = useCanvas ? LineSeriesCanvas : LineSeries;
+    let {yDomain} = this.state;
     return (
       <div data-tid="container">
-        <h1 style={{color:"#152880"}}>Option positions.</h1>
-        <div>
-          <XYPlot
-            width={300}
-            height={300}>
+        <h4 style={{color:"#152880", display: 'flex',  justifyContent:'center', alignItems:'center'}}>Option positions</h4>
+
+          <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
+          <XYPlot width={600} height={300} onMouseLeave={this._onMouseLeave} {...{yDomain}}>
             <HorizontalGridLines />
+            <VerticalGridLines />
+            <XAxis on0={true}/>
+            <YAxis on0={true}/>
+            <ChartLabel
+              text="Price"
+              className="alt-x-label"
+              includeMargin={false}
+              xPercent={0.025}
+              yPercent={1.01}
+            />
+
+            <ChartLabel
+              text="Profit"
+              className="alt-y-label"
+              includeMargin={false}
+              xPercent={0.06}
+              yPercent={0.06}
+              style={{
+                transform: 'rotate(-90)',
+                textAnchor: 'end'
+              }}
+            />
             <LineSeries
-              data={[
-                {x: 1, y: 10},
-                {x: 2, y: 5},
-                {x: 3, y: 15}
-              ]}/>
-            <XAxis />
-            <YAxis />
+              className="first-series"
+              onNearestX={this._onNearestX}
+              data={this.state.chart_data_current}
+            />
+            <LineSeries data={this.state.chart_data_at_zero} />
+            <Crosshair
+              values={this.state.crosshairValues}
+              className={'test-class-name'}
+            />
           </XYPlot>
-        </div>
+          </div>
+
         <Button
           className={classes.button}
-          onClick={()=>this.computeBSM()}
+          onClick={()=>this.plot()}
           variant="outlined"
           // color="primary"
         >Compute</Button>
+
       </div>
     );
   }
